4a8f8a1cd310bf499067e1edda8a811203cacc8f
Vivian Xiao <vivxia456@gmail.com>
2024-04-14 16:26:25 -0400

    added port for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // import the express app
 const webapp = require('./controller/server.js');
 
-const port = 8080;
+const port = process.env.port || 8080;
 // start the web server
 webapp.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`);
