defcf2e5787f62cf155e647ff55d742882a7912d
cryptoderes <allisonzhang1@gmail.com>
2024-04-28 19:19:00 -0400

    temp schedule selector

--- client/src/App.js
+++ client/src/App.js
@@ -5,86 +5,29 @@ import {
   Route,
   BrowserRouter,
 } from "react-router-dom";
-import LoginForm from './pages/LoginForm';
-import RegistrationForm from './pages/RegistrationForm';
-import ResetPass from './pages/ResetPass';
-
-// const LoginForm = () => {
-//   const [credentials, setCredentials] = useState({
-//     username: '',
-//     password: '',
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setCredentials((prevCredentials) => ({
-//       ...prevCredentials,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await fetch('/api/login', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(credentials),
-//       });
-//       if (response.ok) {
-//         alert('Login successful');
-//       } else {
-//         alert('Failed to login');
-//       }
-//     } catch (error) {
-//       console.error('Login error:', error);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <h2>Sign in to when2eat</h2>
-//       <div class='input-form'>
-//         <input
-//           type="text"
-//           name="username"
-//           placeholder="Username"
-//           value={credentials.username}
-//           onChange={handleChange}
-//           required
-//         />
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Password"
-//           value={credentials.password}
-//           onChange={handleChange}
-//           required
-//         />
-//         <button type="submit">Login</button>
-//       </div>
-//       <p>Don't have an account? <a>Create one</a></p>
-//       <a href='gmail.com'>Reset password</a>
-//     </form>
-//   );
-// };
+import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
+import ResetPass from './components/ResetPass';
+import Scheduler from './components/Scheduler';
+import Header from './components/Header';
 
 function App() {
   return (
-    // <div className='App'>
-    //   <body>
-    //     <LoginForm/>
-    //     <RegistrationForm/>
-    //     <ResetPass/>
-    //   </body>
-    // </div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/registration" element={<RegistrationForm/>}/>
-        <Route path="/reset_password" element={<ResetPass/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Header/>
+      <body>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/registration" element={<RegistrationForm/>}/>
+            <Route path="/reset_password" element={<ResetPass/>}/>
+            <Route path="/schedule" element={<Scheduler/>}/>
+          </Routes>
+        </BrowserRouter>
+      </body>
+      
+    </div>
+    
   )
 }
 
