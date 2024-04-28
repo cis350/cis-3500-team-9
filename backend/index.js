c7f8ad6621e39fa6d66f0012505fb616e3ef95a6
chenxyuhua <98442925+chenxyuhua@users.noreply.github.com>
2024-04-28 02:02:09 -0400

    Update localhost to 3050 for backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@
 // import the express app
 const webapp = require('./controller/server.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 // start the web server
 webapp.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`);
