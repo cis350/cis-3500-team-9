712130efa41e9db002f17536060e0b930133b81b
Vivian Xiao <vivxia456@gmail.com>
2024-05-11 16:00:37 -0400

    put login as home page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -9,77 +9,11 @@ import RegistrationForm from '../pages/RegistrationForm';
 import ResetPass from '../pages/ResetPass';
 import Scheduler from '../pages/Scheduler';
 
-// const LoginForm = () => {
-//   const [credentials, setCredentials] = useState({
-//     username: '',
-//     password: '',
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setCredentials((prevCredentials) => ({
-//       ...prevCredentials,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await fetch('/api/login', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(credentials),
-//       });
-//       if (response.ok) {
-//         alert('Login successful');
-//       } else {
-//         alert('Failed to login');
-//       }
-//     } catch (error) {
-//       console.error('Login error:', error);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <h2>Sign in to when2eat</h2>
-//       <div class='input-form'>
-//         <input
-//           type="text"
-//           name="username"
-//           placeholder="Username"
-//           value={credentials.username}
-//           onChange={handleChange}
-//           required
-//         />
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Password"
-//           value={credentials.password}
-//           onChange={handleChange}
-//           required
-//         />
-//         <button type="submit">Login</button>
-//       </div>
-//       <p>Don't have an account? <a>Create one</a></p>
-//       <a href='gmail.com'>Reset password</a>
-//     </form>
-//   );
-// };
-
 function App() {
   return (
-    // <div className='App'>
-    //   <body>
-    //     <LoginForm/>
-    //     <RegistrationForm/>
-    //     <ResetPass/>
-    //   </body>
-    // </div>
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<LoginForm/>}/>
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="/registration" element={<RegistrationForm/>}/>
         <Route path="/reset_password" element={<ResetPass/>}/>
@@ -98,7 +32,7 @@ export default App;
 //   return (
 //     <div className="App">
 //       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
